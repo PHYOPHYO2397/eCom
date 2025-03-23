50cddc3190a0e46e36850a75a1e68f2140d92f6e
PPMT <phyophyomyatthu3@gmail.com>
2025-03-23 19:01:37 +0900

    margin

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -21,7 +21,7 @@ const Product = ({ item }) => {
         <p className="card-text">${price}</p>
         <button
           onClick={() => addToCart(item.id)}
-          className="btn btn-primary position-relative"
+          className="btn btn-primary position-relative me-2"
         >
           Add To Cart
           <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
